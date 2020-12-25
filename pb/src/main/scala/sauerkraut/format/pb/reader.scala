260ea153547bf979a50b00296ee16d313e228bfa
Josh Suereth <Joshua.Suereth@gmail.com>
2020-12-25 14:03:34 -0500

    Record latest banchmark results

--- pb/src/main/scala/sauerkraut/format/pb/reader.scala
+++ pb/src/main/scala/sauerkraut/format/pb/reader.scala
@@ -36,13 +36,40 @@ class DescriptorBasedProtoReader(in: CodedInputStream, repo: TypeDescriptorRepos
       case _ => throw BuildException(s"Unable to deserialize proto to $b", null)
 
   private def pushWithDesc[T](b: core.Builder[T], desc: ProtoTypeDescriptor[T]): core.Builder[T] =
-    (b, desc) match
-      case (b: core.StructureBuilder[T], s: MessageProtoDescriptor[_]) => readStructure(b, s)
-      case (b: core.CollectionBuilder[_,_], s: CollectionTypeDescriptor[_,_]) => pushWithDesc(b.putElement(), s.element.asInstanceOf)
-      case (b: core.PrimitiveBuilder[_], s: PrimitiveTypeDescriptor[_]) => Shared.readPrimitive(in)(b)
-      case _ => throw BuildException(s"Unable to find proto descriptor for $b", null)
+    try
+      b match
+        case b: core.StructureBuilder[T] => readStructure(b, desc.asInstanceOf[MessageProtoDescriptor[T]])
+        case b: core.CollectionBuilder[_,_] => pushWithDesc(b.putElement(), desc.asInstanceOf[CollectionTypeDescriptor[_,_]].element.asInstanceOf)
+        case b: core.PrimitiveBuilder[_] => Shared.readPrimitive(in)(b)
+        case _ => throw BuildException(s"Unsupported builder for protos: $b", null)
+    catch
+      case e: ClassCastException => throw BuildException(s"Builder error.  Builder: $b, Descriptor: $desc", e)
     b
-      
+
+  private inline def readField[T](fieldBuilder: core.Builder[T], desc: ProtoTypeDescriptor[T], wireType: Int): Unit =
+    try 
+      fieldBuilder match {
+        case choice: core.ChoiceBuilder[T] => ???
+        case struct: core.StructureBuilder[T] =>
+          Shared.limitByWireType(in)(WIRETYPE_LENGTH_DELIMITED) {
+            readStructure(struct, desc.asInstanceOf)
+          }
+        case col: core.CollectionBuilder[_,_] =>
+          val colDesc = desc.asInstanceOf[CollectionTypeDescriptor[_,_]]
+          colDesc.element match
+            case x: PrimitiveTypeDescriptor[_] =>
+              if (wireType == WIRETYPE_LENGTH_DELIMITED) 
+                Shared.readCompressedPrimitive(in)(col, x.tag.asInstanceOf)
+                // TODO - directly call putElement here?
+              else pushWithDesc(col, desc)
+            case other =>
+              Shared.limitByWireType(in)(WIRETYPE_LENGTH_DELIMITED) {
+                pushWithDesc(col, desc)
+              }
+        case p: core.PrimitiveBuilder[_] => Shared.readPrimitive(in)(p)
+      }
+    catch
+      case e: ClassCastException => throw BuildException(s"Builder and descriptor do not align.  Builder: $fieldBuilder, Descriptor: $desc", null)
 
   def readStructure[T](struct: core.StructureBuilder[T], mapping: MessageProtoDescriptor[T]): Unit =
     object FieldName:
@@ -50,36 +77,9 @@ class DescriptorBasedProtoReader(in: CodedInputStream, repo: TypeDescriptorRepos
         try Some(mapping.fieldName(num))
         catch 
           case _: MatchError => None
-    def readField(field: String, fieldNum: Int, fieldWireType: Int): Unit =
-      struct.putField(field) match
-        case choice: core.ChoiceBuilder[_] =>
-          ???
-        case struct: core.StructureBuilder[_] =>
-          mapping.fieldDesc(fieldNum) match
-            case msg: MessageProtoDescriptor[_] =>
-              Shared.limitByWireType(in)(WIRETYPE_LENGTH_DELIMITED) {
-                readStructure(struct, msg)
-              }
-            case other => throw BuildException(s"Unable to find descriptor for ${struct.tag}, found $other", null)
-        case col: core.CollectionBuilder[_,_] =>
-          mapping.fieldDesc(fieldNum) match
-            case desc: CollectionTypeDescriptor[_,_] =>
-              desc.element match
-                case x: PrimitiveTypeDescriptor[_] =>
-                  // Special handle compressed primitive arrays.
-                  if (fieldWireType == WIRETYPE_LENGTH_DELIMITED) 
-                    Shared.readCompressedPrimitive(in)(col, x.tag.asInstanceOf)
-                  else pushWithDesc(col, mapping.fieldDesc(fieldNum))
-                case other =>
-                  Shared.limitByWireType(in)(WIRETYPE_LENGTH_DELIMITED) {
-                    pushWithDesc(col, mapping.fieldDesc(fieldNum))
-                  }
-            case other => throw BuildException(s"Unable to find collection descriptor for ${col}, found $other", null)
-        case prim: core.PrimitiveBuilder[_] =>
-          Shared.readPrimitive(in)(prim)
     var done: Boolean = false
     while !done do
       in.readTag match
         case 0 => done = true
         case Tag(wireType, num @ FieldName(field)) =>
-          readField(field, num, wireType)
+          readField(struct.putField(field), mapping.fieldDesc(num), wireType)
